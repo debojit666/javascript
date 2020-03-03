7b7d5c0d33fda0b1113bec4c0395791125949485
Debojit Das <debojit666@gmail.com>
2020-03-03 13:54:11 +0100

    practiced array

---
+++ array.js
@@ -0,0 +1,8 @@
+var friendsAge = [15, 16, 17, 18, 19, 20];
+console.log(friendsAge[0])
+var sonaliAge = friendsAge[3];
+console.log(sonaliAge)
+friendsAge[1] = 21;
+console.log(friendsAge[1])
+var position = friendsAge.indexOf(19);
+console.log(position)
---
+++ hello.js
@@ -0,0 +1,10 @@
+var bananaPrice = 12;
+console.log(bananaPrice);
+var seenAfter = 21;
+console.log(typeof seenAfter);
+
+var name = "Debojit Das";
+console.log(name);
+
+var name = "Debojit Das";
+console.log(typeof name);
