2b54f9276ff4f591e50a746388f6f535f64cc148
Debojit Das <debojit666@gmail.com>
2020-03-03 14:52:05 +0100

    end of for loop

--- forloop.js
+++ forloop.js
@@ -8,3 +8,10 @@ console.log("For loop")
 for(var i=0; i<=10; i++){
     console.log(i);
 } 
+console.log("For loop array")
+var nums = [55, 66, 77, 88, 99];
+
+for(var i=0; i<nums.length; i++){
+    var element = nums[i];
+    console.log(element);
+}
