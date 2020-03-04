edae4f6f6dca097d65a5d6b1c7f3ff0bef146419
Debojit Das <debojit666@gmail.com>
2020-03-04 12:38:11 +0100

    learned to define and call function

---
+++ function.js
@@ -0,0 +1,11 @@
+function playCricket(){
+    console.log('Try to Bat first!');
+    console.log("And score 30 runs.");
+}
+
+playCricket();
+var date = 14;
+var place = "Abahoni field";
+playCricket();
+var practicePlace = "TNT Field";
+playCricket();
