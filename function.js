c81c767fb3549f5088fce38ba805f322ad000869
Debojit Das <debojit666@gmail.com>
2020-03-04 12:52:30 +0100

    function calling with multiple parameters

--- function.js
+++ function.js
@@ -9,3 +9,28 @@ var place = "Abahoni field";
 playCricket();
 var practicePlace = "TNT Field";
 playCricket();
+
+// Function Return
+
+function doubleIt(num){
+    var result = num * 2;
+    return result;
+}
+
+var first = doubleIt(5);
+var second = doubleIt(50);
+var third = doubleIt(500);
+
+var total = first + second + third;
+
+console.log(first, second, third);
+console.log("The total is : ", total);
+
+// Function calling with multiple parameter
+
+function add(num1, num2){
+    var result = num1 + num2;
+    return result;
+}
+total = add(15,17);
+console.log(total);
